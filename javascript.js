8fe444a76d3887ceb95e59fca6af1001da6397f7
Abdulmuizz <yusufabdulmuizz00@gmail.com>
2023-12-11 13:06:55 +0100

    First Commit With Fatima

---
+++ javascript.js
@@ -0,0 +1,97 @@
+// let carName = "Bugatti";
+
+// document.getElementById("demo").innerHTML = carName;
+
+// let x, y, z;
+
+// x = 21;
+// y=3;
+
+// z = x ** y ;
+
+// console.log(z);
+
+
+// function myFunction() {
+//     let y = 10;
+//     let x = 2;
+//     alert(y * x);
+// }
+
+// myFunction();
+
+
+
+// Day 2 with javascript 
+// Working with Objects, Arrays, Conditionals and Loops.
+
+// const myCar = {
+// CarName: 'Toyota Yaris',
+// model: "2006",
+// color:"Grey",
+// };
+
+// Concatination 
+// The name of my car is ....... mode/ ....... with tthe colorr .....
+
+// alert(" The name of my car is " + myCar.CarName + " " + " Model" + myCar.model + " " + " with the color" + myCar.color );
+
+// (myCar.CarName);
+
+
+
+// const restaurant = ["Cabana","Tulip","Taj","Crossover","Mamaput","amalaConnect","kejeGrills","KFC"]
+
+// let addition = restaurant.push("Bamboo");
+
+// console.log(restaurant);
+
+
+
+
+// z = Boolean (x == y);
+
+// console.log(z);
+
+
+
+
+// let x, y, z;
+
+// x = 21;
+// y = 9;
+
+
+// var date = new Date();
+// var time = date.getHours();
+
+// if(time !== 12){
+//     console.log('Good Morning Fatima');
+// } else{
+//     console.log('Good afternoon Fatima');
+// }
+
+// console.log(time);
+
+
+
+const restaurant = ["Cabana","Tulip","Taj","Crossover","Mamaput","amalaConnect","kejeGrills","KFC","Cabana"]
+
+let text = " ";
+
+for (let i = 0; i < restaurant.length; i++) {
+    text += restaurant[i] + "<br>";
+}
+
+document.getElementById('array').innerHTML = text;
+
+
+
+
+
+
+
+// Week 2 
+
+
+
